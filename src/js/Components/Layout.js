e6a6068f4c50a1b7dcd935b6a38864acfcd9cfff
Tynnee <victorasantekyei@yahoo.co.uk>
2017-04-12 19:30:21 +0200

    fix live reload shoutout

--- src/js/Components/Layout.js
+++ src/js/Components/Layout.js
@@ -1,25 +1,32 @@
-/**
+ /**
  * Created by victorasante-kyei on 10/04/2017.
  */
 import React from "react";
 
-import Search from "./search/Search";
-import SearchResult from "./search/SearchResult"
+import Movies from "./search/Movies";
+import SearchResult from "./search/Characters"
 
 export default class Layout extends React.Component {
 	constructor() {
 		super();
 		this.state = {welcome: "Welcome to Star Wars Trivia!"};
+		this.state= {shoutout: "No Shoutouts"};
 	}
+
+	createNewShoutOut(shoutout){
+		this.setState({shoutout});
+	}
+
 	render () {
 		const searchQuestion = "Have you finished your search?";
 		const ResultQuestion = "Are you pleased with your results";
 		return (
 			<div>
 				<h1>{this.state.welcome}</h1>
-				<Search question = {searchQuestion}/>
-				<SearchResult question = {ResultQuestion}/>
+				<h4>Newest Shoutout: {this.state.shoutout}!!</h4>
+				<Movies shoutout={this.state.shoutout}  createNewShoutOut={this.createNewShoutOut.bind(this)} question={searchQuestion}/>
+				<SearchResult question={ResultQuestion}/>
 			</div>
-		)
+		);
 	}
 }
--- src/js/Components/search/Movies.js
+++ src/js/Components/search/Movies.js
@@ -5,11 +5,17 @@
 import React from "react";
 
 export default class Search extends React.Component {
+	createShoutOut(e){
+		const shoutOut = e.target.value;
+		this.props.createNewShoutOut(shoutOut);
+	}
 	render() {
 		return (
 			<div>
 			<h2>Movies List</h2>
 			<h4>{this.props.question}</h4>
+				<h4>Current Shoutout - {this.props.shoutout}!!!!;</h4>
+				<input onChange={this.createShoutOut.bind(this)}/>
 			</div>
 		);
 	}
